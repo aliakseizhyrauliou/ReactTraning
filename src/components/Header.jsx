b88e0867d8611a91509751ddd7f08266d4a13b29
Журавлев Алексей <alehandro.200351@gmail.com>
2022-04-11 15:17:44 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://seeklogo.com/images/S/svg-logo-C2C6DE140E-seeklogo.com.png' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+        <div>
+          <img src='https://molbert.com.ua/img/gallery/big/picture_8364_9539.jpg' />
+        </div>
+        <div>Ava</div>
+        <div>
+          my posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>Post1</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
